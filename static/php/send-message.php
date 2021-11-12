c89039d1e7a4334a4dbb8d60b26d125d6c0de439
Maurizio Paglia <mpaglia0@gmail.com>
2021-11-12 18:45:36 +0100

    Update send-message.php
    First test

--- static/php/send-message.php
+++ static/php/send-message.php
@@ -5,9 +5,11 @@
 $name = strip_tags(htmlspecialchars($_POST['name']));             // comments AND contact form
 $email_address = strip_tags(htmlspecialchars($_POST['email']));   // comments AND contact form
 $subject = strip_tags(htmlspecialchars($_POST['subject']));       // contact form
+$subject = '=?UTF-8?B?'.base64_encode($subject).'?=';             // Encode Subject in UTF-8
 $website = strip_tags(htmlspecialchars($_POST['website']));       // comments form
 $title = strip_tags(htmlspecialchars($_POST['arttitle']));        // comments form
 $message = strip_tags(htmlspecialchars($_POST['message']));       // comments AND contact form
+$message .= "Content-Type: text/html; charset=UTF-8n";            // Encode Mesage in UTF-8
 
 // Create the email and send the message
 
@@ -33,45 +35,3 @@ else {
 }
 
 ?>
-
-
-
-
-
-
-
-<?php
-// (A) UTF-8 MAIL FUNCTION
-function umail ($mailTo, $mailCc, $mailBcc, $mailSubject, $mailBody) {
-  // (A1) MAIL TO
-  if (is_array($mailTo)) { $mailTo = implode(", ", $mailTo); }
-
-  // (A2) MAIL HEADERS
-  $mailHead = [
-    "MIME-Version: 1.0",
-    "Content-type: text/html; charset=utf-8"
-  ];
-
-  // (A3) ADD CC & BCC
-  if ($mailCc !== null) {
-    if (is_array($mailCc)) { $mailCc = implode(", ", $mailCc); }
-    $mailHead[] = "Cc: $mailCc";
-  }
-  if ($mailBcc !== null) {
-    if (is_array($mailBcc)) { $mailBcc = implode(", ", $mailBcc); }
-    $mailHead[] = "Bcc: $mailBcc";
-  }
-
-  // (A4) SEND MAIL
-  return mail($mailTo, $mailSubject, $mailBody, implode("\r\n", $mailHead));
-}
-
-// (B) SEND TEST MAIL
-echo umail (
-  "job@doe.com", // TO
-  ["joe@doe.com", "jon@doe.com"], // CC
-  ["jon@doe.com", "joy@doe.com"], // BCC
-  "Test Email", // SUBJECT
-  "<html><body>This is <strong>強い</strong>.</body></html>", // BODY
-) ? "OK" : "ERROR";
-
