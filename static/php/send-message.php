8cac8d365a83284ce2b0aa02ce6ad1d48f4adcb4
Maurizio Paglia <mpaglia0@gmail.com>
2021-12-18 16:36:28 +0100

    Add files via upload

--- static/php/send-message.php
+++ static/php/send-message.php
@@ -2,31 +2,30 @@
 
 // Collect all variables coming from the contact module OR the comments form
 
-$name = strip_tags(htmlspecialchars($_POST['name']));             // comments AND contact form
-$email_address = strip_tags(htmlspecialchars($_POST['email']));   // comments AND contact form
-$subject = strip_tags(htmlspecialchars($_POST['subject']));       // contact form
-$subject = '=?UTF-8?B?'.base64_encode($subject).'?=';             // Encode Subject in UTF-8
-$website = strip_tags(htmlspecialchars($_POST['website']));       // comments form
-$title = strip_tags(htmlspecialchars($_POST['arttitle']));        // comments form
-$message = strip_tags(htmlspecialchars($_POST['message']));       // comments AND contact form
-$message .= "Content-Type: text/html; charset=UTF-8n";            // Encode Mesage in UTF-8
+$name = strip_tags(htmlspecialchars($_POST['name']));             // comments AND contact
+$email_address = strip_tags(htmlspecialchars($_POST['email']));   // comments AND contact
+$subject = strip_tags(htmlspecialchars($_POST['subject']));       // contact
+$website = strip_tags(htmlspecialchars($_POST['website']));       // comments
+$title = strip_tags(htmlspecialchars($_POST['arttitle']));        // comments
+$message = strip_tags(htmlspecialchars($_POST['message']));       // comments AND contact
 
 // Create the email and send the message
 
-if(empty($title)) {
-   $to = 'your@email.address';
-   $email_subject = "New Contact!";
-   $email_body = "You have a new message!\n\n"."Here below the details:\n\nName: $name\n\nEmail: $email_address\n\nSubject: $subject\n\nMessage:\n$message";
-   mail($to,$email_subject,$email_body);
+if(empty($website)) {
+   $to = 'info@mauriziopaglia.it';
+   $email_subject = "Nuovo Contatto";
+   $email_body = "Hai ricevuto un nuovo messaggio!\n\n"."Ecco i dettagli:\n\nNome: $name\n\nEmail: $email_address\n\nOggetto: $subject\n\nMessaggio:\n$message";
+   mail($to, $email_subject, $email_body);
+   header('Location: /grazie.html');
 return true;
 }
 
 elseif(empty($subject)) {
-
-   $to = 'your@email.address';
-   $email_subject = "New Comment";
-   $email_body = "You have a new comment!.\n\n"."Here below the details:\n\nName: $name\n\nEmail: $email_address\n\nArticle title: $title\n\nWebsite: $website\n\nMessage:\n$message";
+   $to = 'info@mauriziopaglia.it';
+   $email_subject = "Nuovo Commento";
+   $email_body = "Hai ricevuto un nuovo commento!.\n\n"."Ecco i dettagli:\n\nNome: $name\n\nEmail: $email_address\n\nTitolo articolo: $title\n\nSito web: $website\n\nMessaggio:\n$message";
    mail($to,$email_subject,$email_body);
+   header('Location: /grazie.html');
 return true;
 }
 
