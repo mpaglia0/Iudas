029f1d391c0da9c6d3b16c21de5e54042dd5a0c5
Maurizio Paglia <mpaglia0@gmail.com>
2022-06-12 15:52:35 +0200

    Added a simple SPAM filter capability

--- static/php/send-message.php
+++ static/php/send-message.php
@@ -1,5 +1,10 @@
 <?php
 
+// Try to stop SPAM with the honeypot method
+if($_POST['phone_number']) {
+   exit();
+}
+
 // Collect all variables coming from the contact module OR the comments form
 
 $name = strip_tags(htmlspecialchars($_POST['name']));             // comments AND contact
@@ -13,29 +18,29 @@ $message = strip_tags(htmlspecialchars($_POST['message']));       // comments AN
 
 if(empty($website)) {
    header('Content-type: text/html; charset=utf-8');
-   $to = 'your@email.here';
-   $email_subject = "New Contact";
+   $to = 'info@mauriziopaglia.it';
+   $email_subject = "Nuovo Contatto";
    $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
    $headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
    $headers .= 'From: ' . $email_address;
-   $email_body = "You received a new message!\n\n"."Here below the details:\n\nName: $name\n\nSubject: $subject\n\nMessage:\n$message";
+   $email_body = "Hai ricevuto un nuovo messaggio!\n\n"."Ecco i dettagli:\n\nNome: $name\n\nOggetto: $subject\n\nMessaggio:\n$message";
    $body_encoded = base64_encode($email_body);
    mail($to, $email_subject, $body_encoded, $headers);
-   header('Location: /thank_you_page.html');
+   header('Location: /grazie.html');
 return true;
 }
 
 elseif(empty($subject)) {
    header('Content-type: text/html; charset=utf-8');
-   $to = 'your@email.here';
-   $email_subject = "New Comment";
+   $to = 'info@mauriziopaglia.it';
+   $email_subject = "Nuovo Commento";
    $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
    $headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
    $headers .= 'From: ' . $email_address;
-   $email_body = "You received a new comment!.\n\n"."Here below the details:\n\nName: $name\n\nArticle title: $title\n\nWebsite: $website\n\nMessage:\n$message";
+   $email_body = "Hai ricevuto un nuovo commento!.\n\n"."Ecco i dettagli:\n\nNome: $name\n\nTitolo articolo: $title\n\nSito web: $website\n\nMessaggio:\n$message";
    $body_encoded = base64_encode($email_body);
    mail($to, $email_subject, $body_encoded, $headers);
-   header('Location: /thank_you_page.html');
+   header('Location: /grazie.html');
 return true;
 }
 
