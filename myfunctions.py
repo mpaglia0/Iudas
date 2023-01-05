79dfa0ac9232e0cc8a597e79c91d000477de596e
Maurizio Paglia <mpaglia0@gmail.com>
2023-01-05 14:10:53 +0100

    Create myfunctions.py

---
+++ myfunctions.py
@@ -0,0 +1,11 @@
+# custom Jinja2 filter for localizing theme
+def gettext(string, lang):
+    if lang == "en":
+        return string
+    elif lang == "it":
+        if string == "Archives": return "Archivi"
+        elif string == "Archives for": return "Archivi per"
+	    elif string == "Posted by": return "Pubblicato da"
+        ...
+	...
+        else: return string
