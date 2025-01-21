bdd67f1bc5b36e29fb9580f1fc58f481a5e81044
[Maurizio Paglia] <[maurizio@mauriziopaglia.it]>
2025-01-21 18:59:22 +0100

    File deleted

--- static/php/new-ideas.php
+++
@@ -1,8 +0,0 @@
- <?php
-$myfile = fopen("newideas.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-?> 
