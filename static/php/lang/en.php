9f5fd743eec86ecf7d2e93276b4bb6d0cf3d654b
[Maurizio Paglia] <[maurizio@mauriziopaglia.it]>
2025-01-26 12:33:17 +0100

    English language for contact module and comments

---
+++ static/php/lang/en.php
@@ -0,0 +1,13 @@
+<?php
+	$CommentSubjectTXT = "New Comment";
+	$CommentBodyTXT = "You have a new comment!";
+	$CommonDetailsTXT = "Here are the details:";
+	$CommonNameTXT = "Name:";
+	$CommonEmailTXT = "Email:";
+	$CommentArticleTitleTXT = "Article title:";
+	$CommentWebsiteTXT = "Website:";
+	$CommonMessageTXT = "Message:";
+	$ContactSubjectTXT = "New Contact";
+	$ContactBodyTXT = "You have a new message!";
+	$ContactSubjectBodyTXT = "Subject:";
+?>
