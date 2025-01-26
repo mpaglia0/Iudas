1c35b2a2920568f0b665ff96f025c8f1401822ab
[Maurizio Paglia] <[maurizio@mauriziopaglia.it]>
2025-01-26 12:34:32 +0100

    Italian language for contact module and comments

---
+++ static/php/lang/it.php
@@ -0,0 +1,14 @@
+<?php
+	$CommentSubjectTXT = "Nuovo Commento";
+	$CommentBodyTXT = "Hai ricevuto un nuovo commento!";
+	$CommonDetailsTXT = "Ecco i dettagli:";
+	$CommonNameTXT = "Nome:";
+	$CommonEmailTXT = "Email:";
+	$CommentArticleTitleTXT = "Titolo articolo:";
+	$CommentWebsiteTXT = "Sito web:";
+	$CommonMessageTXT = "Messaggio:";
+	$ContactSubjectTXT = "Nuovo Contatto";
+	$ContactBodyTXT = "Hai ricevuto un nuovo messaggio!";
+	$ContactSubjectBodyTXT = "Oggetto:";
+	$CommonErrorMessage = "Il messaggio NON è stato consegnato... Errore:"
+?>
